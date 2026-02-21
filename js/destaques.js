2afefb038a190ec330de37cf9a08e17c7429c3dd
Yuri Silva <yuri.silva@softcomtec.info>
2026-02-21 13:55:01 -0300

    ajustado botão de sacola

--- js/destaques.js
+++ js/destaques.js
@@ -150,10 +150,10 @@ function renderizarCarrossel(produtos) {
                             <span>${produto.material}</span>
                         </div>` : ''}
                     </div>` : ''}
-                    <button class="btn-whatsapp-destaque" onclick="abrirWhatsApp('${produto.nome.replace(/'/g, "\\'")}', '${imagem}')">
-                        ${whatsappIcon}
-                        Consultar no WhatsApp
-                    </button>
+                    <a class="btn-ver-produto-destaque" href="produto.html?id=${produto.id}">
+                        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M6 2L3 6v14a2 2 0 002 2h14a2 2 0 002-2V6l-3-4z"/><line x1="3" y1="6" x2="21" y2="6"/><path d="M16 10a4 4 0 01-8 0"/></svg>
+                        Ver Produto
+                    </a>
                 </div>
             </div>
         </div>`;
